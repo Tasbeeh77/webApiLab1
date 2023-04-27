ffbfd253020325778ad9a38c0b794f7e877bd0cd
Tasbeeh Eissa <tasbeehgamal7@gmail.com>
2023-04-27 18:08:13 +0200

    add regions

--- webApiDay1/Controllers/CarsController.cs
+++ webApiDay1/Controllers/CarsController.cs
@@ -10,92 +10,114 @@ namespace webApiDay1.Controllers
     [ApiController]
     public class CarsController : ControllerBase
     {
-        private static List<Car> cars = new List<Car>
-        {
-            new Car { Id = 1, Name = "Toyota", Model = "Camry", ProductionDate = new DateTime(2019, 1, 1) },
-            new Car { Id = 2, Name = "Honda", Model = "Accord", ProductionDate = new DateTime(2020, 2, 2) },
-            new Car { Id = 3, Name = "Ford", Model = "Mustang", ProductionDate = new DateTime(2018, 3, 3) }
-        };
+        #region Cars collection
+
+            private static List<Car> cars = new List<Car>
+            {
+                new Car { Id = 1, Name = "Toyota", Model = "Camry", ProductionDate = new DateTime(2019, 1, 1) },
+                new Car { Id = 2, Name = "Honda", Model = "Accord", ProductionDate = new DateTime(2020, 2, 2) },
+                new Car { Id = 3, Name = "Ford", Model = "Mustang", ProductionDate = new DateTime(2018, 3, 3) }
+            };
+        #endregion
+
         [HttpGet]
-        public ActionResult<List<Car>> GetAll()
-        {
-            return cars; 
-        }
+        #region Get all Cars Data
+            public ActionResult<List<Car>> GetAll()
+            {
+                return cars; 
+            }
+
+        #endregion
 
         [HttpGet]
         [Route("{id}")]
-        public ActionResult<Car> GetById(int id)
-        {
-            Car? Car = cars.FirstOrDefault(m => m.Id == id);
-            if (Car is null)
+        #region Get car data by id
+            public ActionResult<Car> GetById(int id)
             {
-                return NotFound(new GeneralResponse("Car isn't found"));
+                Car? Car = cars.FirstOrDefault(m => m.Id == id);
+                if (Car is null)
+                {
+                    return NotFound(new GeneralResponse("Car isn't found"));
+                }
+                return Car; 
             }
-            return Car; 
-        }
+
+        #endregion
 
         [HttpPost]
         [Route("Old")]
-        public ActionResult AddOld(Car car)
-        {
-            car.Id = cars.Count + 1;
-            car.Type = "Gas";
-            cars.Add(car);
-            return CreatedAtAction("GetById", new { id = car.Id },
-            new GeneralResponse("car has been added successfully"));
-        }
+        #region add car deprecated version
+            public ActionResult AddOld(Car car)
+            {
+                car.Id = cars.Count + 1;
+                car.Type = "Gas";
+                cars.Add(car);
+                return CreatedAtAction("GetById", new { id = car.Id },
+                new GeneralResponse("car has been added successfully"));
+            }
+
+        #endregion
 
         [HttpPost]
         [Route("New")]
         [ValidateCarType]
-        public ActionResult AddNew(Car car)
-        {
-            car.Id = cars.Count + 1;
-            cars.Add(car);
-            return CreatedAtAction("GetById", new { id = car.Id },
-            new GeneralResponse("car has been added successfully"));
-        }
+        #region add car new version
+            public ActionResult AddNew(Car car)
+            {
+                car.Id = cars.Count + 1;
+                cars.Add(car);
+                return CreatedAtAction("GetById", new { id = car.Id },
+                new GeneralResponse("car has been added successfully"));
+            }
+        #endregion
 
         [HttpPut]
         [Route("{id}")]
         [ValidateCarType]
-        public ActionResult Update(Car CarFromRequest, int id)
-        {
-            if (id != CarFromRequest.Id)
+        #region Update cars data
+            public ActionResult Update(Car CarFromRequest, int id)
             {
-                return BadRequest(new GeneralResponse("This car doesn't Exist"));
+                if (id != CarFromRequest.Id)
+                {
+                    return BadRequest(new GeneralResponse("This car doesn't Exist"));
+                }
+                Car? CarToEdit = cars.FirstOrDefault(m => m.Id == id);
+                if (CarToEdit is null)
+                {
+                    return NotFound();
+                }
+                CarToEdit.Name = CarFromRequest.Name;
+                CarToEdit.Model = CarFromRequest.Model;
+                CarToEdit.ProductionDate = CarFromRequest.ProductionDate;
+                CarToEdit.Type = CarFromRequest.Type;
+                return NoContent();
             }
-            Car? CarToEdit = cars.FirstOrDefault(m => m.Id == id);
-            if (CarToEdit is null)
-            {
-                return NotFound();
-            }
-            CarToEdit.Name = CarFromRequest.Name;
-            CarToEdit.Model = CarFromRequest.Model;
-            CarToEdit.ProductionDate = CarFromRequest.ProductionDate;
-            CarToEdit.Type = CarFromRequest.Type;
-            return NoContent();
-        }
+        #endregion
 
         [HttpDelete]
         [Route("{id}")]
-        public ActionResult Delete(int id)
-        {
-            Car? CarToDelete = cars.FirstOrDefault(m => m.Id == id);
-            if (CarToDelete is null)
+        #region Delete car
+            public ActionResult Delete(int id)
             {
-                return NotFound();
+                Car? CarToDelete = cars.FirstOrDefault(m => m.Id == id);
+                if (CarToDelete is null)
+                {
+                    return NotFound();
+                }
+                cars.Remove(CarToDelete);
+                return NoContent();
             }
-            cars.Remove(CarToDelete);
-            return NoContent();
-        }
+
+        #endregion
 
         [HttpGet]
         [Route("GetCount")]
-        public IActionResult GetRequestsCount()
-        {
-            return Ok(Program.requestsCount.ToString());
-        }
+        #region Get Requests count
+            public IActionResult GetRequestsCount()
+            {
+                return Ok(Program.requestsCount.ToString());
+            }
+        #endregion
 
 /*  [HttpGet]
     [Route("GetCount")]
